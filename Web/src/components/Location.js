04054d682045b837347bc8d88eafcc6598a8252d
Lim_JiSeon <dpf226@naver.com>
2022-09-30 13:16:04 +0900

    select 생성

--- Web/src/components/Location.js
+++ Web/src/components/Location.js
@@ -1,13 +1,59 @@
 import React from "react";
 import ReactiveButton from "reactive-button";
+import { useState } from "react";
+import { hangjungdong } from "./Hangjungdong";
 import { useNavigate, Outlet } from "react-router-dom";
 
 function Location() {
     let navigate = useNavigate();
   
+    const [val1, setVal1] = useState("");
+    const [val2, setVal2] = useState("");
+    const [val3, setVal3] = useState("");
+    const { sido, sigugun, dong } = hangjungdong;
+
     return (
       <div>
-        location
+        <div className="select-container">
+          <div className="select-address">
+            <select onChange={(e) => setVal1(e.target.value)}>
+              <option value="">선택</option>
+              {sido.map((el) => (
+                <option key={el.sido} value={el.sido}>
+                  {el.codeNm}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="select-address">     
+            <select onChange={(e) => setVal2(e.target.value)}>
+              <option value="">선택</option>
+              {sigugun
+                .filter((el) => el.sido === val1)
+                .map((el) => (
+                  <option key={el.sigugun} value={el.sigugun}>
+                    {el.codeNm}
+                  </option>
+                ))}
+            </select>
+          </div>
+
+          <div className="select-address">
+            <select onChange={(e) => setVal3(e.target.value)}>
+              <option value="">선택</option>
+              {dong
+                .filter((el) => el.sido === val1 && el.sigugun === val2)
+                .map((el) => (
+                  <option key={el.dong} value={el.dong}>
+                    {el.codeNm}
+                  </option>
+                ))}
+            </select>
+          </div>
+
+        </div>
+        
         <ReactiveButton
           idleText="다음으로"
           loadingText="Loading"
