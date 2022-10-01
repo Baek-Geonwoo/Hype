6abdc51fb35a5f61873bd73731b010da730df5af
Lim_JiSeon <dpf226@naver.com>
2022-10-02 03:41:33 +0900

    map.js 틀 완성(지도 적용 완료)

--- Web/src/components/Map.js
+++ Web/src/components/Map.js
@@ -4,6 +4,7 @@ import ReactiveButton from "reactive-button";
 import { useNavigate, Outlet } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMap } from '@fortawesome/free-regular-svg-icons'
+import {faCircle} from '@fortawesome/free-solid-svg-icons'
 
 function Map(props) {
     let navigate = useNavigate();
@@ -18,14 +19,28 @@ function Map(props) {
           </h2>
         </div>
 
-        <div className="map-container">
-          
+        <div className="map-content">
+          <div className="map-map">
+            <DispersionMap location={props.locationText}></DispersionMap>
+          </div>  
+
+          <div className="map-container">
+            <h4>TOP 3 상권</h4>
+            <div>
+              <FontAwesomeIcon id="circle1" icon={faCircle} size="1x"/>
+              <span>상권 1</span>
+            </div>
+            <div>
+              <FontAwesomeIcon id="circle2" icon={faCircle} size="1x"/>
+              <span>상권 2</span>
+            </div>
+            <div>
+              <FontAwesomeIcon id="circle3" icon={faCircle} size="1x"/>
+              <span>상권 3</span>
+            </div>
+          </div>
         </div>
 
-        <div className="map-map">
-          <DispersionMap></DispersionMap>
-        </div>  
-      
         <div className="replayBtn">
           <ReactiveButton
             idleText="처음으로"
