172439a40a7941aab2990c32c79e6556cdd14ea5
Lim_JiSeon <dpf226@naver.com>
2022-10-01 20:02:39 +0900

    map 위젯 추가

--- Web/src/components/KakaoMap.js
+++ Web/src/components/KakaoMap.js
@@ -1,25 +1,37 @@
-/* global kakao */
-import { useRef, useEffect } from "react";
+/*global kakao */
+import React, { useEffect } from "react";
 
 function KakaoMap() {
-  const { kakao } = window;
-  const container = useRef(null)
-  const options={
-    center : new kakao.maps.LatLng(33.450701, 126.570667),
-    level : 5
-  }
-
+  
   useEffect(() => {
-    new kakao.maps.Map(container.current, options)
-    return () => {};
+    mapscript();
   }, []);
 
-  return(
-    <div id="map" ref={ container } style={{
-      width:"50vw",
-      height:"50vh"
-    }}></div>
-  )
+  const mapscript = () => {
+    let row = 33.450701;
+    let column = 126.570667;
+    
+    let container = document.getElementById("map");
+    let options = {
+      center: new kakao.maps.LatLng(row, column),
+      level: 5,
+    };
+
+    const map = new kakao.maps.Map(container, options);
+
+    let markerPosition = new kakao.maps.LatLng(
+      row,
+      column
+    );
+
+    let marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+
+    marker.setMap(map);
+  };
+
+  return <div id="map" style={{ width: "50vw", height: "40vh" }}></div>;
 }
 
 export default KakaoMap;
